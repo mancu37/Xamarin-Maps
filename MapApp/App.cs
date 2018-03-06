793f62826ba53bf12425a6cc560fc8fa19b1b1f1
martin <mancu37@gmail.com>
2018-03-06 18:36:32 -0300

    Initial commit

---
+++ MapApp/App.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace MapApp
+{
+    public class App : Application
+    {
+        public App()
+        {
+            var tabs = new TabbedPage();
+
+            // demonstrates the map control with zooming and map-types
+            tabs.Children.Add(new MapPage { Title = "Map/Zoom", Icon = "glyphish_74_location.png" });
+
+            // demonstrates the map control with zooming and map-types
+            tabs.Children.Add(new PinPage { Title = "Pins", Icon = "glyphish_07_map_marker.png" });
+
+            // demonstrates the Geocoder class
+            tabs.Children.Add(new GeocoderPage { Title = "Geocode", Icon = "glyphish_13_target.png" });
+
+            //// opens the platform's native Map app
+            tabs.Children.Add(new MyCustomMap { Title = "Custom Map", Icon = "glyphish_103_map.png" });
+
+            MainPage = tabs;
+        }
+    }
+}
---
+++ MapApp/MyCustomMap.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Maps;
+
+namespace MapApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyCustomMap : ContentPage
+    {
+        Geocoder geoCoder;
+
+        public MyCustomMap()
+        {
+            InitializeComponent();
+
+            geoCoder = new Geocoder();
+
+        }
+
+        async private void btn_Clicked(object sender, EventArgs e)
+        {
+            var xamarinAddress = txtEntry.Text;
+
+            var approximateLocation = await geoCoder.GetPositionsForAddressAsync(xamarinAddress);
+
+            if(approximateLocation.Any())
+            {
+                var item = approximateLocation.Single();
+
+                myCustomMap.Pins.Add(new Pin
+                {
+                    Position = new Position(item.Latitude, item.Longitude),
+                    Label = "Ubicacion re loca..."
+                });
+
+                myCustomMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(item.Latitude, item.Longitude), Distance.FromKilometers(0.3)));
+            }
+
+            
+        }
+    }
+}
